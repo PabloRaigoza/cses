27671957525d5a5afc34423149333864a67b122d
Pablo Raigoza <raigozapablo@gmail.com>
2023-03-05 00:23:24 -0500

    implemented number spiral

--- intro/numberSpiral.cpp
+++ intro/numberSpiral.cpp
@@ -7,9 +7,17 @@ int main() {
 
     int t; cin >> t;
     while (t--) {
-        int y, x; cin >> y >> x;
-        if (x > y) {
-
+        long long y, x; cin >> x >> y;
+        if (x < y) {
+            long long diagonal = y*(y-1)+1;
+            if (y % 2 == 0) cout << diagonal - y + x << '\n';
+            else cout << diagonal + y - x << '\n';
+        } else if (x > y) {
+            long long diagonal = x*(x-1)+1;
+            if (x % 2 == 0) cout << diagonal + x - y << '\n';
+            else cout << diagonal - x + y << '\n';
+        } else {
+            cout << x*(x-1)+1 << '\n';
         }
     }
 }
